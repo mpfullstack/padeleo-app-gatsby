942b430231f1059d5e1cdc2a85e4d830eb8e5d33
mpfullstack <info@marcperez.cat>
2021-02-06 17:59:15 +0100

    Changed material ui default font theme

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import { Provider } from "react-redux";
+import { ThemeProvider } from '@material-ui/core/styles';
 import { PersistGate } from "redux-persist/integration/react";
 import IndexLayout from "../layouts";
 import store, { persistor } from "../redux/store";
 import CreateMatchPage from "../features/matches/createMatchPage";
+import theme from "../theme";
 
 const IndexPage = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <IndexLayout>
-          <CreateMatchPage />
-        </IndexLayout>
+        <ThemeProvider theme={theme}>
+          <IndexLayout>
+            <CreateMatchPage />
+          </IndexLayout>
+        </ThemeProvider>
       </PersistGate>
     </Provider>
   );
