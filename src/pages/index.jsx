a45d628fac22c88df84d6c519c96fca63297fa21
mpfullstack <info@marcperez.cat>
2021-02-06 17:24:55 +0100

    Redux persist setup done

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,18 @@
 import * as React from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import IndexLayout from "../layouts";
-import store from "../redux/store";
+import store, { persistor } from "../redux/store";
 import CreateMatchPage from "../features/matches/createMatchPage";
 
 const IndexPage = () => {
   return (
     <Provider store={store}>
-      <IndexLayout>
-        <CreateMatchPage />
-      </IndexLayout>
+      <PersistGate loading={null} persistor={persistor}>
+        <IndexLayout>
+          <CreateMatchPage />
+        </IndexLayout>
+      </PersistGate>
     </Provider>
   );
 };
