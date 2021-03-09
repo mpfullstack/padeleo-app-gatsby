e53336f776dadc84d1485435be89d46192384caa
mpfullstack <info@marcperez.cat>
2021-03-09 09:21:57 +0100

    Do not redirect to default language

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         // language file path
         defaultLanguage: languageConfig.defaultLanguage,
         // option to redirect to `/ko` when connecting `/`
-        redirect: true,
+        redirect: false,
       },
     },
     {
