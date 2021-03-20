c95a16ca6c7d7ea39f3bd8415e00df1e0f9ee48b
mpfullstack <info@marcperez.cat>
2021-03-20 09:17:18 +0100

    Announce new features in home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,54 @@
 import * as React from "react";
-import { useIntl, navigate } from "gatsby-plugin-intl";
+import { useIntl, navigate, Link } from "gatsby-plugin-intl";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import IndexLayout from "../layouts";
 import Button from "../modules/common/components/Button";
 import { createMatch } from "../features/matches/matchesSlice";
+import check from "../images/check.png";
 // import Helpers from "../helpers";
 
 const mapStateToProps = () => ({});
 const mapDispatchToProps = { createMatch };
 
-const IntroWrapper = styled.div`
+const IntroWrapper = styled.section`
   display: flex;
   align-items: center;
   flex-direction: column;
-  padding-top: 40px;
-  .text {
-    width: 85%;
-    font-size: 22px;
-    text-align: center;
+  padding-top: 10px;
+  padding-right: 15px;
+  padding-left: 15px;
+  .title {
+    font-size: 20px;
+  }
+  .feature {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    padding-left: 25px;
+    &::before {
+      content: ' ';
+      background: url(${check}) 0 0 no-repeat;
+      position: absolute;
+      left: 0;
+      top: 30px;
+      width: 32px;
+      height: 32px;
+    }
+    .text {
+      width: 85%;
+      font-size: 18px;
+    }
+  }
+  .create-match {
+    margin-top: 30px;
+    margin-bottom: 10px;
+  }
+  .my-matches {
+    margin-top: 10px;
+    color: ${props => props.theme.palette.primary.main};
+    margin-bottom: 40px;
   }
 `;
 
@@ -27,14 +57,24 @@ const Intro = connect(mapStateToProps, mapDispatchToProps)(
     const intl = useIntl();
     return (
       <IntroWrapper>
-        <h1>{intl.formatMessage({ id: "hiPadeleros" })}</h1>
-        <p className="text">{intl.formatMessage({ id: "createMathIntroduction" })}</p>
+        <h1 className="title">{intl.formatMessage({ id: "padelMatchManagement" })}</h1>
+        <article className="feature">
+          <p className="text">{intl.formatMessage({ id: "createMathIntroduction" })}</p>
+        </article>
+        <article className="feature">
+          <p className="text">{intl.formatMessage({ id: "costManagementIntroduction" })}</p>
+        </article>
+        <article className="feature">
+          <p className="text">{intl.formatMessage({ id: "calendarManagementIntroduction" })}</p>
+        </article>
         <Button color="primary" className="create-match" onClick={() => {
           createMatch();
           navigate(`/matches`);
         }}>
           {intl.formatMessage({ id: "createMatch" })}
         </Button>
+        <span>{intl.formatMessage({ id: "or" })}</span>
+        <Link className="my-matches" to="/matches">{intl.formatMessage({ id: "goToYourMatches"})}</Link>
       </IntroWrapper>
     );
   }
