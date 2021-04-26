47d15467150cb35689712ae27c4db9de45abdc80
mpfullstack <info@marcperez.cat>
2021-04-26 22:59:06 +0200

    Styling booked and not-booked court

--- src/modules/matches/components/CourtBooked.jsx
+++ src/modules/matches/components/CourtBooked.jsx
@@ -1,18 +1,34 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 import { useIntl } from "gatsby-plugin-intl";
+
+const StyledParagraph = styled.p`
+  &.booked {
+    color: #3a7c16;
+  }
+  &.not-booked {
+    color: #ad2b2b;
+  }
+`;
 
 const CourtBooked = ({ clubName, courtBooked }) => {
   const intl = useIntl();
 
   if (clubName) {
+    const classNames = ["sub-text"];
+    if (courtBooked) {
+      classNames.push("booked");
+    } else {
+      classNames.push("not-booked");
+    }
     return (
-      <p className="sub-text">
+      <StyledParagraph className={classNames.join(" ")}>
         ({courtBooked ?
           intl.formatMessage({ id: "courtBooked" })
           :
           intl.formatMessage({ id: "courtNotBooked" })})
-      </p>
+      </StyledParagraph>
     );
   } else {
     return null;
